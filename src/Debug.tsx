61fab376ccdc56acc19055251b33d8ad898f31e9
Heiner Behrends <heinerbehrends@hotmail.com>
2025-02-22 22:26:09 +0100

    Add Debug component

---
+++ src/Debug.tsx
@@ -0,0 +1,33 @@
+import { LoopPlayerContext } from "./ContextProvider";
+import { Snapshot } from "./machine";
+
+const { useSelector } = LoopPlayerContext;
+
+function getStateAndContext(snapshot: Snapshot) {
+  return {
+    state: snapshot.value,
+    context: snapshot.context,
+  };
+}
+
+export function Debug() {
+  const { state, context } = useSelector(getStateAndContext);
+  return (
+    <div>
+      <pre>
+        {JSON.stringify(
+          {
+            audioFile: context.audioFile,
+            timelineLeft: context.timelineLeft,
+            timelineWidth: context.timelineWidth,
+            dragXOffset: context.dragXOffset,
+            track: state.track,
+            dragTime: state.dragTime,
+          },
+          null,
+          2
+        )}
+      </pre>
+    </div>
+  );
+}
