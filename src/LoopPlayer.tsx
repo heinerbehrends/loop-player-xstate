e83ad5812e7bf876bf84fcf87fc57293eba6b59c
Heiner Behrends <heinerbehrends@hotmail.com>
2025-02-22 22:33:27 +0100

    Add debug, improve styling in LoopPlayer

--- src/LoopPlayer.tsx
+++ src/LoopPlayer.tsx
@@ -1,14 +1,16 @@
 import { AudioElement } from "./AudioElement";
+import { Debug } from "./Debug";
 import { PlayButton } from "./PlayButton";
 import { TimeLine } from "./TimeLine";
 
 export function LoopPlayer() {
   return (
-    <main className="flex flex-col items-center h-screen max-w-[960px] mx-auto">
-      <h1>Loop Player</h1>
+    <main className="flex flex-col gap-4 pt-6 items-center h-screen max-w-[960px] mx-auto">
+      <h1 className="text-3xl font-light">Loop Player</h1>
       <TimeLine />
       <PlayButton />
       <AudioElement />
+      <Debug />
     </main>
   );
 }
