4025c2ee1433634cbbeaba4274f147cf7bfd57f5
Heiner Behrends <heinerbehrends@hotmail.com>
2025-02-22 01:26:26 +0100

    Add keyboard seek

--- src/TimeLine.tsx
+++ src/TimeLine.tsx
@@ -1,30 +1,34 @@
-import { useState, useEffect, useRef } from "react";
-import { Snapshot } from "./AudioElement";
+import { useEffect, useRef } from "react";
 import { LoopPlayerContext } from "./ContextProvider";
+import { Event, Snapshot } from "./machine";
 
 const { useSelector, useActorRef } = LoopPlayerContext;
 
-function getTimelineData(snapshot: Snapshot) {
-  return {
-    currentTime: snapshot.context.currentTime,
-    duration: snapshot.context.ref?.duration ?? 0,
-    timelineWidth: snapshot.context.timelineWidth,
-  };
-}
-
 export function TimeLine() {
-  const timelineRef = useRef<HTMLDivElement>(null);
-  const { currentTime, duration, timelineWidth } = useSelector(getTimelineData);
+  const timelineRef = useRef<HTMLButtonElement>(null);
+  const { currentTime, duration, timelineWidth, timelineLeft } =
+    useSelector(getTimelineData);
+  const { send } = useActorRef();
   const progress = currentTime / duration;
   const offset = timelineWidth * progress;
 
   useTimelineLoaded(timelineRef);
 
   return (
-    <div ref={timelineRef} className="w-full grid grid-cols-1 grid-rows-1 h-6">
-      <div className="w-full bg-gray-100 row-start-1 col-start-1" />
-      <div className="flex w-full row-start-1 col-start-1 ">
-        <button
+    <button
+      ref={timelineRef}
+      data-testid="timeline"
+      className="w-full grid grid-cols-1 grid-rows-1 h-6 cursor-pointer"
+      onKeyDown={(event) =>
+        handleKeyDown({ event, currentTime, duration, send })
+      }
+      onMouseDown={(event) =>
+        handleMouseDown({ event, timelineLeft, timelineWidth, duration, send })
+      }
+    >
+      <div className="w-full bg-gray-100 row-start-1 col-start-1 h-full" />
+      <div className="flex w-full row-start-1 col-start-1 h-full">
+        <div
           className="h-6 w-6 bg-pink-600 rounded-full absolute z-10"
           style={{
             transform: `translateX(calc(${offset}px - 12px))`,
@@ -43,26 +47,121 @@ export function TimeLine() {
           aria-label="Audio progress"
         />
       </div>
-    </div>
+    </button>
   );
 }
 
+function getTimelineData(snapshot: Snapshot) {
+  return {
+    currentTime: snapshot.context.currentTime,
+    duration: snapshot.context.ref?.duration ?? 0,
+    timelineWidth: snapshot.context.timelineWidth,
+    timelineLeft: snapshot.context.timelineLeft,
+  };
+}
+
+type HandleMouseDownProps = {
+  event: React.MouseEvent<HTMLButtonElement>;
+  timelineLeft: number;
+  timelineWidth: number;
+  duration: number;
+  send: (action: Event) => void;
+};
+
+function handleMouseDown({
+  event,
+  timelineLeft,
+  timelineWidth,
+  duration,
+  send,
+}: HandleMouseDownProps) {
+  const time = ((event.clientX - timelineLeft) / timelineWidth) * duration;
+  send({
+    type: "SEEK",
+    time,
+  });
+}
+
+type HandleKeyDownProps = {
+  event: React.KeyboardEvent<HTMLButtonElement>;
+  currentTime: number;
+  duration: number;
+  send: (event: Event) => void;
+};
+
+function handleKeyDown({
+  event,
+  currentTime,
+  duration,
+  send,
+}: HandleKeyDownProps) {
+  const keyToTimeMap = createSeekTimeMap(duration);
+  if (event.key in keyToTimeMap) {
+    const time = keyToTimeMap[event.key as keyof typeof keyToTimeMap];
+    send({
+      type: "SEEK",
+      time,
+    });
+  }
+  if (event.key === "ArrowLeft") {
+    const time = event.shiftKey
+      ? Math.max(0, currentTime - 2)
+      : Math.max(0, currentTime - 10);
+    send({
+      type: "SEEK",
+      time,
+    });
+  }
+  if (event.key === "ArrowRight") {
+    const time = event.shiftKey
+      ? Math.min(duration, currentTime + 2)
+      : Math.min(duration, currentTime + 10);
+    send({
+      type: "SEEK",
+      time,
+    });
+  }
+}
+
+function createSeekTimeMap(duration: number) {
+  return {
+    "0": 0,
+    "1": duration * 0.1,
+    "2": duration * 0.2,
+    "3": duration * 0.3,
+    "4": duration * 0.4,
+    "5": duration * 0.5,
+    "6": duration * 0.6,
+    "7": duration * 0.7,
+    "8": duration * 0.8,
+    "9": duration * 0.9,
+  };
+}
+
 function useTimelineLoaded(
-  timelineRef: React.RefObject<HTMLDivElement | null>
-): DOMRect {
-  const [rect, setRect] = useState<DOMRect | undefined>();
+  timelineRef: React.RefObject<HTMLButtonElement | null>
+) {
   const { send } = useActorRef();
   useEffect(() => {
     if (!timelineRef?.current) {
       return;
     }
     const rect = timelineRef.current?.getBoundingClientRect();
-    setRect(rect);
     send({
       type: "TIMELINE_LOADED",
       timelineLeft: rect?.left ?? 0,
       timelineWidth: rect?.width ?? 1,
     });
   }, [send, timelineRef]);
-  return rect ?? new DOMRect(0, 0, 1, 1);
 }
+
+export type TimelineLoadedEvent = {
+  type: "TIMELINE_LOADED";
+  timelineLeft: number;
+  timelineWidth: number;
+};
+
+export type SeekEvent = {
+  type: "SEEK";
+  time: number;
+};
